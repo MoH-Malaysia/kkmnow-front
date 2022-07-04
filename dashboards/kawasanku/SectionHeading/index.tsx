fdd1ba4ecefc6e379ad006dc8b8516aa89763d93
chris0610 <chrislow360@gmail.com>
2022-07-04 17:07:59 +0800

    add kawasanku section heading component

---
+++ dashboards/kawasanku/SectionHeading/index.tsx
@@ -0,0 +1,19 @@
+import { useTranslation } from "next-i18next";
+import { FunctionComponent } from "react";
+
+type SectionHeadingProps = {
+  children?: React.ReactNode;
+};
+
+const SectionHeading: FunctionComponent<SectionHeadingProps> = ({ children }) => {
+  const { t } = useTranslation("kawasanku");
+
+  return (
+    <div className="mb-5 flex w-full flex-col items-start justify-between gap-2 md:mb-7 md:flex-row md:items-center md:gap-0">
+      <h4>{children}</h4>
+      <p className="text-sm text-gray-400">{t("census_2020")}</p>
+    </div>
+  );
+};
+
+export default SectionHeading;
