4c7e6fcc53bdca9b9d631964a50eaa35339e209b
chris0610 <chrislow360@gmail.com>
2022-07-04 17:08:26 +0800

    update kawasanku dashboard

--- pages/dashboard/kawasanku/index.tsx
+++ pages/dashboard/kawasanku/index.tsx
@@ -3,33 +3,32 @@ import { useTranslation } from "next-i18next";
 import type { InferGetStaticPropsType } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
-import type { Page } from "@lib/types";
+import type { Page, ReactElement } from "@lib/types";
 
-import { Hero, Container } from "@components/index";
+import KawasankuLayout from "dashboards/kawasanku/Layout";
+import SectionHeading from "dashboards/kawasanku/SectionHeading";
 
 const Kawasanku: Page = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation("kawasanku");
 
   return (
     <>
-      <Hero background="bg-[#EDF8ED]" className="flex flex-col gap-8 text-dim">
-        <div className="flex flex-col gap-2">
-          <p className="text-sm font-bold uppercase">{t("kawasanku")}</p>
-          <h3 className="text-black">{t("header")}</h3>
-          <div className="max-w-hero">
-            <p>{t("description1")}</p>
-            <p>{t("description2")}</p>
-          </div>
-        </div>
-        <p className="text-sm font-bold">{t("filter_title")}</p>
-      </Hero>
-      <Container className="min-h-screen"> </Container>
+      <SectionHeading>
+        {t("section1_title1")} <span className="capitalize underline">{t("malaysia")}</span>{" "}
+        {t("section1_title2")}
+      </SectionHeading>
+      <SectionHeading>{t("section2_title1")}</SectionHeading>
+      <SectionHeading>{t("section3_title")}</SectionHeading>
     </>
   );
 };
 
+Kawasanku.layout = (page: ReactElement) => {
+  return <KawasankuLayout>{page}</KawasankuLayout>;
+};
+
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
-  const translation = await serverSideTranslations(locale!, ["kawasanku"]);
+  const translation = await serverSideTranslations(locale!, ["common", "kawasanku"]);
 
   return {
     props: {
