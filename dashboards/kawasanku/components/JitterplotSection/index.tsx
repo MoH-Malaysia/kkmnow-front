60488d56d4821cfd35f997630b4bc3c74d77ecab
chris0610 <chrislow360@gmail.com>
2022-07-05 16:42:16 +0800

    add spotlight component to jitterplot section (pending API)

--- dashboards/kawasanku/components/JitterplotSection/index.tsx
+++ dashboards/kawasanku/components/JitterplotSection/index.tsx
@@ -1,11 +1,12 @@
 import dynamic from "next/dynamic";
-import { FunctionComponent } from "react";
 import { useTranslation } from "next-i18next";
+import { FunctionComponent, useState } from "react";
 
 import { OptionType } from "@components/types";
 import { IJitterplots } from "@dashboards/kawasanku/lib/types";
 import { AREA_TYPES } from "@dashboards/kawasanku/lib/constants";
 
+import Spotlight from "./Spotlight";
 import SectionHeading from "../SectionHeading";
 
 const Jitterplots = dynamic(() => import("./Jitterplots"), { ssr: false });
@@ -13,21 +14,22 @@ const Jitterplots = dynamic(() => import("./Jitterplots"), { ssr: false });
 type JitterplotSectionProps = {
   areaType: AREA_TYPES;
   data: IJitterplots;
-  comparisons: OptionType[];
   currentLocation?: OptionType;
 };
 
 const JitterplotSection: FunctionComponent<JitterplotSectionProps> = ({
   areaType,
   data,
-  comparisons,
   currentLocation,
 }) => {
   const { t } = useTranslation("kawasanku");
 
+  const [comparisons, setComparisons] = useState<OptionType[]>([]);
+
   return (
     <div className="py-6">
       <SectionHeading>{t("section2_title1")}</SectionHeading>
+      <Spotlight comparisons={comparisons} setComparisons={setComparisons} />
       <Jitterplots areaType={areaType} data={data} comparisons={comparisons} />
     </div>
   );
