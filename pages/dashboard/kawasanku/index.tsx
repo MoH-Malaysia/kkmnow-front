cd492c006d7361c3f984df4707139d1b93514d55
chris0610 <chrislow360@gmail.com>
2022-07-05 12:04:22 +0800

    add choropleth section

--- pages/dashboard/kawasanku/index.tsx
+++ pages/dashboard/kawasanku/index.tsx
@@ -5,21 +5,26 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 
 import type { Page, ReactElement } from "@lib/types";
 
-import KawasankuLayout from "dashboards/kawasanku/Layout";
-import SectionHeading from "dashboards/kawasanku/SectionHeading";
+import KawasankuLayout from "@dashboards/kawasanku/components/Layout";
+import SectionHeading from "@dashboards/kawasanku/components/SectionHeading";
+import ChoroplethSection from "@dashboards/kawasanku/components/ChoroplethSection";
 
 const Kawasanku: Page = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
   const { t } = useTranslation("kawasanku");
 
   return (
-    <>
-      <SectionHeading>
-        {t("section1_title1")} <span className="capitalize underline">{t("malaysia")}</span>{" "}
-        {t("section1_title2")}
-      </SectionHeading>
-      <SectionHeading>{t("section2_title1")}</SectionHeading>
-      <SectionHeading>{t("section3_title")}</SectionHeading>
-    </>
+    <div className="divide-y">
+      <div className="py-6">
+        <SectionHeading>
+          {t("section1_title1")} <span className="capitalize underline">{t("malaysia")}</span>{" "}
+          {t("section1_title2")}
+        </SectionHeading>
+      </div>
+      <div className="py-6">
+        <SectionHeading>{t("section2_title1")}</SectionHeading>
+      </div>
+      <ChoroplethSection />
+    </div>
   );
 };
 
