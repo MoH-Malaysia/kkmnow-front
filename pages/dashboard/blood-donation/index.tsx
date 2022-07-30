a9ad6f526902225895c6bc237eab1a3cc954e0df
irfanismail <irfan.ismail96@gmail.com>
2022-07-30 21:12:03 +0800

    add: section comp to blood donation pg

--- pages/dashboard/blood-donation/index.tsx
+++ pages/dashboard/blood-donation/index.tsx
@@ -1,4 +1,13 @@
-import { Hero, Container, Tabs, Panel, MenuDropdown, Dropdown, Tooltip } from "@components/index";
+import {
+  Hero,
+  Container,
+  Tabs,
+  Panel,
+  MenuDropdown,
+  Dropdown,
+  Tooltip,
+  Section,
+} from "@components/index";
 import { InferGetStaticPropsType, GetStaticProps } from "next";
 
 import dynamic from "next/dynamic";
@@ -7,50 +16,8 @@ import { post } from "@lib/api";
 const Bar = dynamic(() => import("@dashboards/covidnow/components/Bar"), { ssr: false });
 const Heatmap = dynamic(() => import("@dashboards/covidnow/components/Heatmap"), { ssr: false });
 const BarLine = dynamic(() => import("@dashboards/covidnow/components/BarLine"), { ssr: false });
-const Table = dynamic(() => import("@dashboards/covidnow/components/Table"), { ssr: false });
 
 const BloodDonation = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
-  //   const [currentTab, selectTab] = useState(0);
-  //   const BarTabsMenu = [
-  //     {
-  //       name: "Deaths",
-  //       title: "Deaths per 100K",
-  //     },
-  //     {
-  //       name: "Vent.",
-  //       title: "Ventilator per 100K",
-  //     },
-  //     {
-  //       name: "ICU",
-  //       title: "ICU per 100K",
-  //     },
-  //     {
-  //       name: "Hosp.",
-  //       title: "Hospital per 100K",
-  //     },
-  //     {
-  //       name: "Cases",
-  //       title: "Cases per 100K",
-  //     },
-  //   ];
-  //   const TableTabsMenu = [
-  //     {
-  //       name: "Show All",
-  //     },
-  //     {
-  //       name: "Total",
-  //     },
-  //     {
-  //       name: "Adults",
-  //     },
-  //     {
-  //       name: "Adolescents",
-  //     },
-  //     {
-  //       name: "Children",
-  //     },
-  //   ];
-
   return (
     <>
       <Hero background="hero-light-1">
@@ -69,31 +36,24 @@ const BloodDonation = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
         </div>
       </Hero>
       <Container className="min-h-screen">
-        <section className="border-b py-12">
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
-            <h4>Is Klang Valley’s current blood supply sufficient?</h4>
-            <span className="text-dim">Data as of {new Date().toDateString()}</span>
-          </div>
+        {/* Is {{ area }}'s current blood supply sufficient? */}
+        <Section title="Is Klang Valley's current blood supply sufficient?">
           <div className="grid grid-cols-1 gap-12 py-6 xl:grid-cols-2 ">
-            <BarLine title="Deaths by Date of Death" menu={<MenuDropdown />} />
+            <Heatmap className="h-[500px]" />
             <BarLine title="Patients Ventilated" menu={<MenuDropdown />} />
           </div>
-        </section>
-        <section className="space-y-4 border-b py-12">
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
-            <h4>What are the latest blood donation trends in Klang Valley?</h4>
-            <span className="text-dim">Data as of {new Date().toDateString()}</span>
-          </div>
-          <div className="text-dim">
-            <p>
-              Blood compromises 3 components - red blood cells, platelets, and plasma. Although
+        </Section>
+
+        {/* What are the latest blood donation trends in {{ area }}? */}
+        <Section
+          title="What are the latest blood donation trends in Klang Valley?"
+          description="Blood compromises 3 components - red blood cells, platelets, and plasma. Although
               plasma can be stored for up to 1 year, red blood cells can be only stored for up to 35
               days, and plasma only for up to 5 days. Therefore, it is vital to maintain a high and
               stable level of blood donations; when blood donation activity is low or volatile,
               healthcare services that depend upon blood transfusions may start to come under
-              stress.
-            </p>
-          </div>
+              stress."
+        >
           <div className="flex w-full flex-col gap-12">
             <BarLine title="Daily Donations" menu={<MenuDropdown />} />
 
@@ -104,59 +64,39 @@ const BloodDonation = ({}: InferGetStaticPropsType<typeof getStaticProps>) => {
               <BarLine title="Donation by location" menu={<MenuDropdown />} />
             </div>
           </div>
-        </section>
-        <section className="space-y-4 border-b py-12">
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
-            <h4>How strong is new donor recruitment in Klang Valley?</h4>
-            <span className="text-dim">Data as of {new Date().toDateString()}</span>
-          </div>
-          <div className="text-dim">
-            <p>
-              Recruitment of new donors is vital to replace donors who reach their golden years and
-              stop donating, as well as to support a growing population.
-            </p>
-          </div>
+        </Section>
+
+        {/* How strong is the new donor recruitment in {{ area }}? */}
+        <Section
+          title="How strong is the new donor recruitment in Klang Valley?"
+          description="Recruitment of new donors is vital to replace donors who reach their golden years and
+            stop donating, as well as to support a growing population."
+        >
           <div className="grid w-full grid-cols-1 gap-12 xl:grid-cols-2">
             <Bar className="h-[300px]" title="Number of new donors" menu={<MenuDropdown />} />
             <Bar className="h-[300px]" title="New donors by age group" menu={<MenuDropdown />} />
           </div>
-        </section>
-        <section className="space-y-4 border-b py-12">
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
-            <h4>What proportion of the population in Klang Valley donates blood?</h4>
-            <span className="text-dim">Data as of {new Date().toDateString()}</span>
-          </div>
-          <div className="text-dim">
-            <p>
-              To ensure a stable and high supply of blood, we need 10% of the eliglble population to
-              donate at least 1 time per year.
-            </p>
-          </div>
+        </Section>
+
+        {/* What proportion of the population in {{ area }} donates blood? */}
+        <Section
+          title="What proportion of the population in Klang Valley donates blood?"
+          description="To ensure a stable and high supply of blood, we need 10% of the eliglble population to
+            donate at least 1 time per year."
+        >
           <div className="grid w-full grid-cols-1 gap-12 xl:grid-cols-2">
             <Heatmap className="h-[500px]" />
             <Heatmap className="h-[500px]" />
           </div>
-        </section>
-        <section className="border-b py-12">
-          <div className="flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between">
-            <h4>How vaccinated against COVID-19 are we?</h4>
-            <span className="text-dim">Data as of {new Date().toDateString()}</span>
-          </div>
-          <div>
-            {/* <Tabs
-              className="flex flex-wrap justify-end gap-2"
-              title="Vaccination Progress by State"
-            >
-              {TableTabsMenu.map((menu, index) => {
-                return (
-                  <Panel key={index} title={menu.name}>
-                    <Table />
-                  </Panel>
-                );
-              })}
-            </Tabs> */}
-          </div>
-        </section>
+        </Section>
+
+        {/* How is this data collected? */}
+        <Section
+          title="How is this data collected?"
+          description="Map showing locations of BBIS centres:"
+        >
+          <p>Map goes here</p>
+        </Section>
       </Container>
     </>
   );
