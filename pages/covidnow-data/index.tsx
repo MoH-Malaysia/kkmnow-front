7f35284b1a48983fcf2f64bc90a675b658a175c0
manfye <manfye@gmail.com>
2022-09-24 14:59:42 +0800

    add some todo

--- pages/covidnow-data/index.tsx
+++ pages/covidnow-data/index.tsx
@@ -25,13 +25,15 @@ const BloodDonationIndex: Page = ({
 
 export const getStaticProps: GetStaticProps = async ({ locale }) => {
   const i18n = await serverSideTranslations(locale!, ["common"]);
-  // const { data } = await get("/kkmnow", { dashboard: "blood_donation", state: "mys" }); // fetch static data here
+
   const data2 = await get("/kkmnow", { dashboard: "covid_now" }); // fetch static data here
+
   // transfrom:
   Object.values(data2.data.heatmap_chart).forEach((item: any) => {
     item.data = item.data.filter((_item: any) => _item.y !== null);
   });
-  // pending array sorting for monday to sunday
+  // TODO: Toogle between table for choropleth map
+  // TODO: pending array sorting for monday to sunday
   console.log(data2);
 
   return {
@@ -40,13 +42,6 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
       timeseries_chart: data2.data.timeseries_chart,
       heatmap_chart: Object.values(data2.data.heatmap_chart),
       barmeter_chart: data2.data.bar_chart,
-      // timeseries_all: data.timeseries_all,
-      // timeseries_bloodstock: data.timeseries_bloodstock,
-      // heatmap_donorrate: data.heatmap_donorrate,
-      // heatmap_bloodstock: Object.values(data.heatmap_bloodstock),
-      // heatmap_retention: Object.values(data.heatmap_retention),
-      // barchart_age: data.barchart_age,
-      // barchart_time: data.barchart_time,
     },
   };
 };
