8c509879d27f6934c087fe98216ffd35017c18fc
manfye <manfye@gmail.com>
2022-10-01 09:58:33 +0800

    initialized both GA and clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,29 @@ class MyDocument extends Document {
             integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14="
             crossOrigin=""
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_GA_KEYS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_CLARITY_KEYS}");
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
