3ef9fbba9105c6bfedda8aeb1cfb912db4ee87f6
manfye <manfye@gmail.com>
2022-10-01 20:40:07 +0800

    add photo and update manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -55,38 +55,87 @@ class MyDocument extends Document {
           />
 
           {/* PWA setting */}
-          <meta name="application-name" content="PWA App" />
+          <meta name="application-name" content="KKMNow" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-          <meta name="apple-mobile-web-app-title" content="PWA App" />
-          <meta name="description" content="Best PWA App in the world" />
+          <meta name="apple-mobile-web-app-title" content="KKMNow" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+          {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
 
-          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-          <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-          <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+          <link rel="apple-touch-icon" href="/static/images/icons/touch-icon-iphone.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/static/images/icons/touch-icon-ipad.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/static/images/icons/touch-icon-iphone-retina.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="167x167"
+            href="/static/images/icons/touch-icon-ipad-retina.png"
+          />
 
-          <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/images/icons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/images/icons/favicon-16x16.png"
+          />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+          {/* <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" /> */}
           <link rel="shortcut icon" href="/favicon.ico" />
-          {/* 
-          // Apple Splash Screen
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-          <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> 
-          */}
+
+          {/* // Apple Splash Screen */}
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_2048.png"
+            sizes="2048x2732"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_1668.png"
+            sizes="1668x2224"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_1536.png"
+            sizes="1536x2048"
+          />
+
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_1125.png"
+            sizes="1125x2436"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_1242.png"
+            sizes="1242x2208"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_750.png"
+            sizes="750x1334"
+          />
+          <link
+            rel="apple-touch-startup-image"
+            href="/static/images/icons/apple_splash_640.png"
+            sizes="640x1136"
+          />
         </Head>
         <body>
           <Main />
