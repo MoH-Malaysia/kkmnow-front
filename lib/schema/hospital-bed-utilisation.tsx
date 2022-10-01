f23414d2ff55e0755798e97787ed617b7d21a462
irfanismail <irfan.ismail96@gmail.com>
2022-10-01 14:46:03 +0800

    fix: sort desc first

--- lib/schema/hospital-bed-utilisation.tsx
+++ lib/schema/hospital-bed-utilisation.tsx
@@ -25,6 +25,7 @@ export const HOSPITAL_TABLE_SCHEMA = (onClick?: (item: any) => void) => [
     header: "Name",
     accessorKey: "data.hospital",
     id: "data.hospital",
+    sortDescFirst: true,
     cell: (item: any) => (
       <div className="text-left">
         <span
@@ -44,17 +45,20 @@ export const HOSPITAL_TABLE_SCHEMA = (onClick?: (item: any) => void) => [
     subheader: "Non-critical care",
     accessorKey: "data.beds_nonicu",
     id: "beds_nonicu",
+    sortDescFirst: true,
   },
   {
     header: "ICU Beds",
     subheader: "Critical care",
     accessorKey: "data.beds_icu",
     id: "name",
+    sortDescFirst: true,
   },
   {
     header: "Hospital Bed Utilisation",
     accessorFn: (item: any) => item.data.util_nonicu && +item.data.util_nonicu.toFixed(1),
     id: "util_nonicu",
+    sortDescFirst: true,
     scale: true,
     unit: "%",
   },
