33029b055fc85b3a1d9f3c13b0943357afa903dd
manfye <manfye@gmail.com>
2022-10-01 19:15:37 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -77,6 +77,16 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
           <link rel="shortcut icon" href="/favicon.ico" />
+          {/* 
+          // Apple Splash Screen
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
+          <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' /> 
+          */}
         </Head>
         <body>
           <Main />
