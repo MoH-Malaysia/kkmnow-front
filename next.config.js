b9a65af82166161a7d25813cccf0bb3ed9f74a37
manfye <manfye@gmail.com>
2022-10-01 10:09:33 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,4 +5,5 @@ const withGraphql = require("next-plugin-graphql");
 module.exports = withGraphql({
   i18n,
   reactStrictMode: false, // Bug requires strict-mode false: https://github.com/plouc/nivo/issues/2009
+  poweredByHeader: false, // Remove powered by header
 });
