7f7d64e2719aa133e3813ffeff3194de0fece6e5
Amaluddin Asyraf <amaluddinasyraf@gmail.com>
2025-09-29 15:51:32 +0800

    remove unused nextra package

--- packages/datagovmy-nextra/src/components/table.tsx
+++
@@ -1,5 +0,0 @@
-import type { ComponentProps } from "react";
-
-export const Table = ({ className = "", ...props }: ComponentProps<"table">) => (
-  <table className={"block overflow-x-scroll " + className} {...props} />
-);
